9360c0e0cc4e2cde627be9405c5e82c74f857588
Ayusht20 <ayushtrilokchandani2005@gmail.com>
2026-07-12 15:33:22 +0530

    Made some changes in the frontend for connecting with backend on proper url!

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -30,7 +30,7 @@ export default function AuthPage() {
     const endpoint = isRegister ? '/auth/register' : '/auth/login';
     
     try {
-      const response = await fetch(`https://tally-lhy7.onrender.com/${endpoint}`, {
+      const response = await fetch(`https://tally-lhy7.onrender.com${endpoint}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
